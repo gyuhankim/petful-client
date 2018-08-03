8a7e56369e538c48ba56deaca20f2ba938a51c05
giovanni <mormagli@gmail.com>
2018-08-03 11:39:39 -0500

    wrote cat actions

--- src/actions/cat.js
+++ src/actions/cat.js
@@ -1,36 +1,70 @@
 
 
 
-export function fetchCat(){
-    
+
+export const fetchCat = () => dispatch =>{
+    dispatch(fetchingCat())
+    fetch('/cat').then(res => {
+        if (!res.ok) {
+            return Promise.reject(res.statusText);
+        }
+        return res.json();
+    }).then(cat => {
+        dispatch(catFetchSuccess(cat));
+    }).catch(err => {
+        dispatch(catFetchError(err));
+    });
+
 }
 
-
+export const FETCHING_CAT = 'FETCHING_CAT'
 export function fetchingCat(){
+    type: FETCHING_CAT
+}
+
+export const CAT_FETCH_ERROR = 'CAT_FETCH_ERROR'
+export function catFetchError(err){
+    type: CAT_FETCH_ERROR,
+    err
+}
+
+export const CAT_FETCH_SUCCESS = 'CAT_FETCH_SUCCESS'
+export function catFetchSuccess(cat){
+    type: CAT_FETCH_SUCCESS,
+    cat
+}
+
+
+
+export const adoptCat = () => dispatch =>{
+    dispatch(adoptingCat())
+    fetch('/cat', {method: 'DELETE'})
+        .then(res => {
+        if (!res.ok) {
+            return Promise.reject(res.statusText);
+        }
+        return res.json();
+    }).then(cat => {
+        dispatch(catAdoptionSuccess(cat));
+    }).catch(err => {
+        dispatch(catAdoptionError(err));
+    });
 
 }
 
-export function catFetchError(){
-
-}
-
-export function catFetchSuccess(){
-
-}
-
-
-
-export function adoptCat(){}
-
-
+export const ADOPTING_CAT = 'ADOPTING_CAT'
 export function adoptingCat(){
-
+    type: ADOPTING_CAT
 }
 
-export function catAdoptionError(){
-
+export const CAT_ADOPTION_ERROR = 'CAT_ADOPTION_ERROR'
+export function catAdoptionError(err){
+    type: catAdoptionError,
+    err
 }
 
-export function catAdoptionSuccess(){
-
+export const CAT_ADOPTION_SUCCESS = 'CAT_ADOPTION_SUCCESS'
+export function catAdoptionSuccess(cat){
+    type: catAdoptionSuccess,
+    cat
 }
